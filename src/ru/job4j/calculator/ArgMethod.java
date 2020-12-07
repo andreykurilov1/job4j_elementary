9a6bdf7d01e553150c9c4d832910bceb76260aad
andreykurilov1 <andrey_kurilov1@mail.ru>
2020-12-07 22:16:57 +0300

    4.1. Аргументы [#415382]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -11,6 +11,9 @@ public class ArgMethod {
     public static void hello(int age) {
         System.out.println(age);
     }
+    public static void hello(String name1,String name2,String name3) {
+        System.out.println(name1 + " " + name2 + " " + name3);
+    }
     public static void main(String[] args) {
         String name = "Andrey Kurilov";
         int age = 26;
@@ -21,7 +24,7 @@ public class ArgMethod {
 
         ArgMethod.hello(age);
 
-        ArgMethod.hello(name + name + name);
+        ArgMethod.hello(name, name, name);
 
     }
 }
